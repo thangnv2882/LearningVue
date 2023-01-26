db2f773ed07dbb2ea584019fd68bc057d4d46d73
Nguyen Van Thang <thang.nv2882@gmail.com>
2023-01-26 23:42:48 +0700

    vue components

---
+++ hello-vue/src/components/CompFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <h1>{{ text }}</h1>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "comp-footer",
+  data() {
+    return {
+      text: "Footer"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
